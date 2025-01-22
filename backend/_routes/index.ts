a498bf1429430f42e63a7221de5949dc11fd8d54
tezlm <zestylemonade@celery.eu.org>
2025-01-22 08:39:51 -0800

    remove old backend

--- backend/_routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { OpenAPIHono } from "@hono/zod-openapi";
-import { HonoEnv } from "../data.ts";
-import setupRooms from "./impl/rooms.ts";
-
-export function setup(app: OpenAPIHono<HonoEnv>) {
-	setupRooms(app);
-}
--- backend/types.ts
+++
@@ -1,3 +0,0 @@
-export * from "./types/common.ts";
-export * as db from "./types/db.ts";
-export * as sync from "./types/sync.ts";
