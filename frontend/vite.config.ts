186d651991bb0feb03dbe8a2ae9c92a441c2427b
tezlm <zestylemonade@celery.eu.org>
2025-01-27 16:08:06 -0800

    build sourcemaps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import deno from "@deno/vite-plugin";
 import solid from "vite-plugin-solid";
-// import { nodePolyfills } from "vite-plugin-node-polyfills";
-// import sass from "sass";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,18 +10,8 @@ export default defineConfig({
 			// watching seems broken on my machine unfortunately
 			usePolling: true,
 		},
-		// headers: {
-		//   'Cross-Origin-Opener-Policy': 'same-origin',
-		//   'Cross-Origin-Embedder-Policy': 'require-corp',
-		// },
-		// fs: {
-		// 	allow: [".", "../node_modules/.deno"],
-		// },
 	},
-	// optimizeDeps: {
-	//   exclude: ['@electric-sql/pglite'],
-	// },
-	// optimizeDeps: {
-	//   exclude: ['@sqlite.org/sqlite-wasm'],
-	// },
+	build: {
+		sourcemap: true,
+	},
 });
