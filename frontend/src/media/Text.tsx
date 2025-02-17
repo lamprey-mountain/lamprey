1a9393fbf6a3dcb052d10bc1db79508263706245
tezlm <zestylemonade@celery.eu.org>
2025-02-17 12:14:03 -0800

    feat: line numbers

--- frontend/src/media/Text.tsx
+++ frontend/src/media/Text.tsx
@@ -1,7 +1,13 @@
-import { createResource, createSignal } from "solid-js";
+import { createResource, createSignal, For, Show } from "solid-js";
 import { MediaProps } from "./util.ts";
+import { useCtx } from "../context.ts";
+import { debounce } from "@solid-primitives/scheduled";
+
+// 16KiB
+const MAX_PREVIEW_SIZE = 16384;
 
 export const TextView = (props: MediaProps) => {
+	const ctx = useCtx();
 	const byteFmt = Intl.NumberFormat("en", {
 		notation: "compact",
 		style: "unit",
@@ -11,11 +17,13 @@ export const TextView = (props: MediaProps) => {
 
 	const ty = () => props.media.source.mime.split(";")[0];
 
+	const [collapsed, setCollapsed] = createSignal(true);
+	const [copied, setCopied] = createSignal(false);
+
 	const [text] = createResource(() => props.media, async (media) => {
 		const req = await fetch(media.source.url, {
 			headers: {
-				// 16KiB
-				"Range": "bytes=0-16384"
+				"Range": `bytes=0-${MAX_PREVIEW_SIZE}`,
 			},
 		});
 		if (!req.ok) throw req.statusText;
@@ -23,15 +31,33 @@ export const TextView = (props: MediaProps) => {
 		return text;
 	});
 
-	const [collapsed, setCollapsed] = createSignal(true);
+	const unsetCopied = debounce(() => setCopied(false), 1000);
+	const copy = () => {
+		const t = text();
+		if (t) {
+			setCopied(true);
+			navigator.clipboard.writeText(t);
+			unsetCopied();
+		} else {
+			ctx.dispatch({ do: "modal.alert", text: "file not loaded yet" });
+		}
+	}
 
 	return (
 		<div class="media-text">
 			<div class="wrap" classList={{ collapsed: collapsed() }}>
-				<pre>{text()}</pre>
-				<button onClick={() => setCollapsed(c => !c)}>
+				<button class="copy" onClick={copy}>{copied() ? "copied!" : "copy"}</button>
+				<pre class="numbered">
+					<For each={text()?.split("\n")}>{l =>
+						<code>{l + "\n"}</code>
+					}</For>
+				</pre>
+				<button onClick={() => setCollapsed((c) => !c)}>
 					{collapsed() ? "expand" : "collapse"}
 				</button>
+				<Show when={props.media.source.size > MAX_PREVIEW_SIZE}>
+					<span class="warn">warning:</span> file preview truncated (too long!)
+				</Show>
 			</div>
 			<a download={props.media.filename} href={props.media.source.url}>
 				download {props.media.filename}
