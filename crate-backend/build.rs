601839a1c3fbf561050b0bf36d4d2c1ceaf2705b
tezlm <zestylemonade@celery.eu.org>
2025-02-22 17:09:45 -0800

    feat: server version endpoint
    fixes #194

--- crate-backend/build.rs
+++ crate-backend/build.rs
@@ -1,4 +1,19 @@
-fn main() {
+use vergen_gix::{BuildBuilder, CargoBuilder, Emitter, GixBuilder, RustcBuilder};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=migrations");
     println!("cargo:rerun-if-changed=sql");
+    println!("cargo:rerun-if-changed=build.rs");
+
+    let build = BuildBuilder::all_build()?;
+    let cargo = CargoBuilder::all_cargo()?;
+    let rustc = RustcBuilder::all_rustc()?;
+    let git = GixBuilder::all_git()?;
+    Emitter::default()
+        .add_instructions(&build)?
+        .add_instructions(&cargo)?
+        .add_instructions(&rustc)?
+        .add_instructions(&git)?
+        .emit()?;
+    Ok(())
 }
--- crate-backend/src/routes/debug.rs
+++ crate-backend/src/routes/debug.rs
@@ -2,13 +2,51 @@ use std::sync::Arc;
 
 use axum::response::IntoResponse;
 use axum::{extract::State, Json};
+use serde::Serialize;
 use types::{UrlEmbed, UrlEmbedRequest};
+use utoipa::ToSchema;
 use utoipa_axum::{router::OpenApiRouter, routes};
 
 use crate::ServerState;
 
 use super::util::Auth;
 use crate::error::Result;
+
+#[derive(Serialize, ToSchema)]
+struct ServerVersion {
+    cargo_debug: bool,
+    cargo_target: &'static str,
+    git_sha: &'static str,
+    git_describe: &'static str,
+    git_branch: &'static str,
+    rustc_semver: &'static str,
+    rustc_llvm: &'static str,
+    rustc_sha: &'static str,
+    rustc_channel: &'static str,
+}
+
+/// Get server version
+#[utoipa::path(
+    get,
+    path = "/debug/version",
+    tags = ["debug"],
+    responses(
+        (status = OK, body = ServerVersion, description = "success"),
+    )
+)]
+pub async fn debug_version() -> Result<impl IntoResponse> {
+    Ok(Json(ServerVersion {
+        cargo_debug: env!("VERGEN_CARGO_DEBUG") == "true",
+        cargo_target: env!("VERGEN_CARGO_TARGET_TRIPLE"),
+        git_sha: env!("VERGEN_GIT_SHA"),
+        git_describe: env!("VERGEN_GIT_DESCRIBE"),
+        git_branch: env!("VERGEN_GIT_BRANCH"),
+        rustc_semver: env!("VERGEN_RUSTC_SEMVER"),
+        rustc_llvm: env!("VERGEN_RUSTC_LLVM_VERSION"),
+        rustc_sha: env!("VERGEN_RUSTC_COMMIT_HASH"),
+        rustc_channel: env!("VERGEN_RUSTC_CHANNEL"),
+    }))
+}
 
 /// Embed a url
 #[utoipa::path(
@@ -29,5 +67,7 @@ pub async fn debug_embed_url(
 }
 
 pub fn routes() -> OpenApiRouter<Arc<ServerState>> {
-    OpenApiRouter::new().routes(routes!(debug_embed_url))
+    OpenApiRouter::new()
+        .routes(routes!(debug_version))
+        .routes(routes!(debug_embed_url))
 }
