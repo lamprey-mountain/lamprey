2975dd0497fef2a3c0d4adba3f59645f8817ecc5
tezlm <zestylemonade@celery.eu.org>
2025-05-13 01:54:02 -0700

    misc: scaffold voice routes

--- crate-backend/src/routes/voice.rs
+++ crate-backend/src/routes/voice.rs
@@ -1,48 +1,130 @@
 use std::sync::Arc;
 
 use axum::{
-    extract::{Path, Query, State},
-    http::StatusCode,
-    response::IntoResponse,
+    extract::{Path, State},
     Json,
 };
-use common::v1::types::{MessageId, MessageThreadUpdate, ThreadState, ThreadType};
-use serde::{Deserialize, Serialize};
-use utoipa::ToSchema;
+use common::v1::types::{ThreadId, UserId};
 use utoipa_axum::{router::OpenApiRouter, routes};
-use validator::Validate;
-
-use crate::{
-    types::{
-        DbMessageCreate, DbThreadCreate, MessageSync, MessageType, MessageVerId, PaginationQuery,
-        PaginationResponse, Permission, RoomId, Thread, ThreadCreate, ThreadId, ThreadPatch,
-    },
-    Error, ServerState,
-};
 
 use super::util::{Auth, HeaderReason};
-use crate::error::Result;
 
-/// Voice do something (TEMP)
+use crate::error::Result;
+use crate::{Error, ServerState};
+
+/// Voice member get (TODO)
 #[utoipa::path(
-    post,
-    path = "/thread/{thread_id}/voice",
-    params(("thread_id", description = "Thread id")),
+    get,
+    path = "/voice/{thread_id}/member/{user_id}",
+    params(
+        ("thread_id", description = "Thread id"),
+        ("user_id", description = "User id"),
+    ),
     tags = ["voice"],
     responses(
-        (status = CREATED, body = Thread, description = "Create thread success"),
+        (status = OK, body = (), description = "ok"),
     )
 )]
-async fn voice_foobar(
-    Path((_room_id,)): Path<(RoomId,)>,
-    Auth(_user_id): Auth,
+async fn voice_member_get(
+    Path((_room_id, _user_id)): Path<(ThreadId, UserId)>,
+    Auth(_auth_user_id): Auth,
     State(_s): State<Arc<ServerState>>,
-    // HeaderReason(_reason): HeaderReason,
-    // Json(_json): Json<ThreadCreate>,
 ) -> Result<Json<()>> {
     Err(Error::Unimplemented)
 }
 
+/// Voice member update (TODO)
+#[utoipa::path(
+    patch,
+    path = "/voice/{thread_id}/member/{user_id}",
+    params(
+        ("thread_id", description = "Thread id"),
+        ("user_id", description = "User id"),
+    ),
+    tags = ["voice"],
+    responses(
+        (status = OK, body = (), description = "ok"),
+    )
+)]
+async fn voice_member_patch(
+    Path((_room_id, _user_id)): Path<(ThreadId, UserId)>,
+    Auth(_auth_user_id): Auth,
+    State(_s): State<Arc<ServerState>>,
+    HeaderReason(_reason): HeaderReason,
+    Json(_json): Json<()>,
+) -> Result<Json<()>> {
+    Err(Error::Unimplemented)
+}
+
+/// Voice member disconnect (TODO)
+#[utoipa::path(
+    delete,
+    path = "/voice/{thread_id}/member/{user_id}",
+    params(
+        ("thread_id", description = "Thread id"),
+        ("user_id", description = "User id"),
+    ),
+    tags = ["voice"],
+    responses(
+        (status = NO_CONTENT, description = "ok"),
+    )
+)]
+async fn voice_member_disconnect(
+    Path((_room_id, _user_id)): Path<(ThreadId, UserId)>,
+    Auth(_auth_user_id): Auth,
+    State(_s): State<Arc<ServerState>>,
+    HeaderReason(_reason): HeaderReason,
+    Json(_json): Json<()>,
+) -> Result<Json<()>> {
+    Err(Error::Unimplemented)
+}
+
+/// Voice member move (TODO)
+#[utoipa::path(
+    post,
+    path = "/voice/{thread_id}/member/{user_id}",
+    params(
+        ("thread_id", description = "Thread id"),
+        ("user_id", description = "User id"),
+    ),
+    tags = ["voice"],
+    responses(
+        (status = OK, body = (), description = "ok"),
+    )
+)]
+async fn voice_member_move(
+    Path((_room_id, _user_id)): Path<(ThreadId, UserId)>,
+    Auth(_auth_user_id): Auth,
+    State(_s): State<Arc<ServerState>>,
+    HeaderReason(_reason): HeaderReason,
+    Json(_json): Json<()>,
+) -> Result<Json<()>> {
+    Err(Error::Unimplemented)
+}
+
+/// Voice region list (TODO)
+#[utoipa::path(
+    get,
+    path = "/voice/region",
+    tags = ["voice"],
+    responses(
+        (status = OK, body = (), description = "ok"),
+    )
+)]
+async fn voice_region_list(State(_s): State<Arc<ServerState>>) -> Result<Json<()>> {
+    Err(Error::Unimplemented)
+}
+
 pub fn routes() -> OpenApiRouter<Arc<ServerState>> {
-    OpenApiRouter::new().routes(routes!(voice_foobar))
+    let router = OpenApiRouter::new();
+
+    // #[cfg(feature = "voice")]
+    let router = router
+        .routes(routes!(voice_member_get))
+        .routes(routes!(voice_member_patch))
+        .routes(routes!(voice_member_disconnect))
+        .routes(routes!(voice_member_move))
+        .routes(routes!(voice_region_list));
+
+    router
 }
