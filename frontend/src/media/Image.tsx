f0e25046326e971079f6915616a935b96b074ada
tezlm <zestylemonade@celery.eu.org>
2025-06-20 03:49:26 -0700

    fix: dont collapse images? hopefully?

--- frontend/src/media/Image.tsx
+++ frontend/src/media/Image.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { createSignal, Show } from "solid-js";
 import { useCtx } from "../context.ts";
 import {
 	byteFmt,
@@ -17,14 +17,25 @@ type ImageViewProps = MediaProps & {
 
 export const ImageView = (props: ImageViewProps) => {
 	const ctx = useCtx();
-
+	console.log(props.media);
 	const [loaded, setLoaded] = createSignal(false);
 	const thumb = () =>
 		getThumb(props.media, props.thumb_width ?? 320, props.thumb_height ?? 320)!;
 	const url = () => getUrl(thumb());
-	const width = () => thumb().width;
-	const height = () => thumb().height;
-	const ty = () => props.media.source.mime.split(";")[0];
+
+	const height = () => {
+		if (props.media.source.type === "Image") {
+			return props.media.source.height;
+		}
+		return 0;
+	};
+
+	const width = () => {
+		if (props.media.source.type === "Image") {
+			return props.media.source.width;
+		}
+		return 0;
+	};
 
 	return (
 		<Resize height={height()} width={width()} ratio={width() / height()}>
@@ -42,34 +53,30 @@ export const ImageView = (props: ImageViewProps) => {
 				}}
 			>
 				<Loader loaded={loaded()} />
-				<img
-					src={url()}
-					alt={props.media.alt ?? undefined}
-					height={height()!}
-					width={width()!}
-					onLoad={[setLoaded, true]}
-					onEmptied={[setLoaded, false]}
-				/>
-				<a
-					class="download"
-					download={props.media.filename}
-					href={getUrl(props.media.source)}
-					onClick={(e) => e.stopPropagation()}
-				>
-					<button>
-						<img src={iconDownload} class="icon" />
-					</button>
-				</a>
-				<footer class="info dim">
-					{props.media.filename} - {byteFmt.format(props.media.source.size)}
-				</footer>
+				<Show when={loaded()}>
+					<img
+						src={url()}
+						alt={props.media.alt ?? undefined}
+						height={height()!}
+						width={width()!}
+						onLoad={[setLoaded, true]}
+						onEmptied={[setLoaded, false]}
+					/>
+					<a
+						class="download"
+						download={props.media.filename}
+						href={getUrl(props.media.source)}
+						onClick={(e) => e.stopPropagation()}
+					>
+						<button>
+							<img src={iconDownload} class="icon" />
+						</button>
+					</a>
+					<footer class="info dim">
+						{props.media.filename} - {byteFmt.format(props.media.source.size)}
+					</footer>
+				</Show>
 			</article>
 		</Resize>
 	);
-	// <header class="info dim">
-	// 	{props.media.filename}
-	// </header>
-	// <footer class="info dim">
-	// 	{ty()} - {byteFmt.format(props.media.source.size)}
-	// </footer>
 };
