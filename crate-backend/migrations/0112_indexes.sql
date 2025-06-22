f79a5f6d3b27703ff5e3f5be2a1776ae544e0e6c
tezlm <zestylemonade@celery.eu.org>
2025-06-22 05:51:06 -0700

    perf: add indexes

--- crate-backend/migrations/0112_indexes.sql
+++ crate-backend/migrations/0112_indexes.sql
@@ -6,6 +6,3 @@ DROP INDEX message_is_latest;
 CREATE INDEX idx_message_latest_filtered 
 ON message(thread_id, deleted_at, id) 
 WHERE deleted_at IS NULL;
-
-CREATE INDEX att_json_version_id ON att_json(version_id);
-CREATE INDEX url_embed_json_version_id ON url_embed_json(version_id);
