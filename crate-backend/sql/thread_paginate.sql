8d15e4513b25f570936a2ad3607fa5f0ca6d2ce2
tezlm <zestylemonade@celery.eu.org>
2025-06-23 11:16:42 -0700

    fix: only return active threads

--- crate-backend/sql/thread_paginate.sql
+++ crate-backend/sql/thread_paginate.sql
@@ -26,5 +26,5 @@ join message_count on message_count.thread_id = thread.id
 join last_id on last_id.thread_id = thread.id
 full outer join usr on true
 left join unread on usr.id = unread.user_id and thread.id = unread.thread_id
-where room_id = $1 AND usr.id = $2 AND thread.id > $3 AND thread.id < $4 and deleted_at is null
+where room_id = $1 AND usr.id = $2 AND thread.id > $3 AND thread.id < $4 and thread.deleted_at is null
 order by (CASE WHEN $5 = 'f' THEN thread.id END), thread.id DESC LIMIT $6
