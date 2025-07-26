b768b5a60fc2a07a330b74e691c252ac6a7e9c3e
tezlm <zestylemonade@celery.eu.org>
2025-07-26 13:23:06 -0700

    feat: verify email page

--- frontend/src/VerifyEmail.tsx
+++ frontend/src/VerifyEmail.tsx
@@ -1,4 +1,4 @@
-import { RouteSectionProps } from "@solidjs/router";
+import { RouteSectionProps, useNavigate } from "@solidjs/router";
 import { useApi } from "./api.tsx";
 import { useCtx } from "./context.ts";
 
@@ -6,10 +6,28 @@ export const RouteVerifyEmail = (
 	props: RouteSectionProps<unknown>,
 ) => {
 	const ctx = useCtx();
+	const nav = useNavigate();
 
 	const verify = () => {
-		console.log("verify email addr", props.location.query.code);
-		alert("todo");
+		ctx.client.http.POST("/api/v1/user/{user_id}/email/{addr}/verify/{code}", {
+			params: {
+				path: {
+					addr: props.location.query.email,
+					code: props.location.query.code,
+					user_id: "@self",
+				},
+			},
+		}).then(({ error }) => {
+			if (error) {
+				console.error(error);
+				alert("error while verifying, see console");
+			} else {
+				nav("/settings/email");
+			}
+		}).catch((err) => {
+			console.error(err);
+			alert("error while verifying, see console");
+		});
 	};
 
 	return (
