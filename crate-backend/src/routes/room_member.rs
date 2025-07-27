bd449abfd94d4c1be5a2888ff7edb76b7bffe5c7
tezlm <zestylemonade@celery.eu.org>
2025-07-27 14:25:28 -0700

    feat: implement ban routes

--- crate-backend/src/routes/room_member.rs
+++ crate-backend/src/routes/room_member.rs
@@ -72,8 +72,10 @@ async fn room_member_get(
     let perms = s.services().perms.for_room(auth_user_id, room_id).await?;
     perms.ensure_view()?;
     let res = d.room_member_get(room_id, target_user_id).await?;
-    // TODO: return `Ban`s
-    if !matches!(res.membership, RoomMembership::Join { .. }) {
+    if !matches!(
+        res.membership,
+        RoomMembership::Join { .. } | RoomMembership::Ban { .. }
+    ) {
         Err(Error::NotFound)
     } else {
         Ok(Json(res))
@@ -274,7 +276,7 @@ async fn room_member_delete(
     Ok(StatusCode::NO_CONTENT)
 }
 
-/// Room ban create (TODO)
+/// Room ban create
 #[utoipa::path(
     put,
     path = "/room/{room_id}/ban/{user_id}",
@@ -287,11 +289,39 @@ async fn room_member_delete(
         (status = NO_CONTENT, description = "success"),
     )
 )]
-async fn room_ban_create() -> Result<()> {
-    Err(Error::Unimplemented)
+async fn room_ban_create(
+    Path((room_id, target_user_id)): Path<(RoomId, UserIdReq)>,
+    Auth(auth_user_id): Auth,
+    HeaderReason(reason): HeaderReason,
+    State(s): State<Arc<ServerState>>,
+) -> Result<impl IntoResponse> {
+    let target_user_id = match target_user_id {
+        UserIdReq::UserSelf => auth_user_id,
+        UserIdReq::UserId(id) => id,
+    };
+    let d = s.data();
+    let perms = s.services().perms.for_room(auth_user_id, room_id).await?;
+    perms.ensure(Permission::MemberBan)?;
+
+    d.room_member_set_membership(room_id, target_user_id, RoomMembership::Ban {})
+        .await?;
+    s.services()
+        .perms
+        .invalidate_room(target_user_id, room_id)
+        .await;
+    s.services().perms.invalidate_is_mutual(target_user_id);
+    let res = d.room_member_get(room_id, target_user_id).await?;
+    s.broadcast_room(
+        room_id,
+        auth_user_id,
+        reason,
+        MessageSync::RoomMemberUpsert { member: res },
+    )
+    .await?;
+    Ok(StatusCode::NO_CONTENT)
 }
 
-/// Room ban remove (TODO)
+/// Room ban remove
 #[utoipa::path(
     delete,
     path = "/room/{room_id}/ban/{user_id}",
@@ -304,11 +334,39 @@ async fn room_ban_create() -> Result<()> {
         (status = NO_CONTENT, description = "success"),
     )
 )]
-async fn room_ban_remove() -> Result<()> {
-    Err(Error::Unimplemented)
+async fn room_ban_remove(
+    Path((room_id, target_user_id)): Path<(RoomId, UserIdReq)>,
+    Auth(auth_user_id): Auth,
+    HeaderReason(reason): HeaderReason,
+    State(s): State<Arc<ServerState>>,
+) -> Result<impl IntoResponse> {
+    let target_user_id = match target_user_id {
+        UserIdReq::UserSelf => auth_user_id,
+        UserIdReq::UserId(id) => id,
+    };
+    let d = s.data();
+    let perms = s.services().perms.for_room(auth_user_id, room_id).await?;
+    perms.ensure(Permission::MemberBan)?;
+
+    d.room_member_set_membership(room_id, target_user_id, RoomMembership::Leave {})
+        .await?;
+    s.services()
+        .perms
+        .invalidate_room(target_user_id, room_id)
+        .await;
+    s.services().perms.invalidate_is_mutual(target_user_id);
+    let res = d.room_member_get(room_id, target_user_id).await?;
+    s.broadcast_room(
+        room_id,
+        auth_user_id,
+        reason,
+        MessageSync::RoomMemberUpsert { member: res },
+    )
+    .await?;
+    Ok(StatusCode::NO_CONTENT)
 }
 
-/// Room ban get (TODO)
+/// Room ban get
 #[utoipa::path(
     get,
     path = "/room/{room_id}/ban/{user_id}",
@@ -318,14 +376,30 @@ async fn room_ban_remove() -> Result<()> {
     ),
     tags = ["room_member"],
     responses(
-        (status = NO_CONTENT, description = "success"),
+        (status = OK, body = RoomMember, description = "success"),
     )
 )]
-async fn room_ban_get() -> Result<()> {
-    Err(Error::Unimplemented)
+async fn room_ban_get(
+    Path((room_id, target_user_id)): Path<(RoomId, UserIdReq)>,
+    Auth(auth_user_id): Auth,
+    State(s): State<Arc<ServerState>>,
+) -> Result<impl IntoResponse> {
+    let target_user_id = match target_user_id {
+        UserIdReq::UserSelf => auth_user_id,
+        UserIdReq::UserId(id) => id,
+    };
+    let d = s.data();
+    let perms = s.services().perms.for_room(auth_user_id, room_id).await?;
+    perms.ensure_view()?;
+    let res = d.room_member_get(room_id, target_user_id).await?;
+    if !matches!(res.membership, RoomMembership::Ban { .. }) {
+        Err(Error::NotFound)
+    } else {
+        Ok(Json(res))
+    }
 }
 
-/// Room ban list (TODO)
+/// Room ban list
 #[utoipa::path(
     get,
     path = "/room/{room_id}/ban",
@@ -335,11 +409,29 @@ async fn room_ban_get() -> Result<()> {
     ),
     tags = ["room_member"],
     responses(
-        (status = NO_CONTENT, description = "success"),
+        (status = OK, body = PaginationResponse<RoomMember>, description = "success"),
     )
 )]
-async fn room_ban_list() -> Result<()> {
-    Err(Error::Unimplemented)
+async fn room_ban_list(
+    Path(room_id): Path<RoomId>,
+    Query(paginate): Query<PaginationQuery<UserId>>,
+    Auth(user_id): Auth,
+    State(s): State<Arc<ServerState>>,
+) -> Result<impl IntoResponse> {
+    let d = s.data();
+    let perms = s.services().perms.for_room(user_id, room_id).await?;
+    perms.ensure_view()?;
+    let res = d.room_member_list(room_id, paginate).await?;
+    let res = PaginationResponse {
+        items: res
+            .items
+            .into_iter()
+            .filter(|m| matches!(m.membership, RoomMembership::Ban { .. }))
+            .collect(),
+        has_more: res.has_more,
+        total: 0, // FIXME
+    };
+    Ok(Json(res))
 }
 
 pub fn routes() -> OpenApiRouter<Arc<ServerState>> {
