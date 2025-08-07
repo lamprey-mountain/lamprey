c1c238ecfe65ba40f5a240db0cfa4700ee586727
tezlm <zestylemonade@celery.eu.org>
2025-08-07 03:56:48 -0700

    fix: use value instead of messagesync for audit log entries

--- crate-common/src/v1/types/audit_logs.rs
+++ crate-common/src/v1/types/audit_logs.rs
@@ -26,13 +26,11 @@ pub struct AuditLog {
 
     #[cfg_attr(feature = "utoipa", schema(no_recursion))]
     /// Generated sync payload (sent in websocket)
-    pub payload: Box<MessageSync>,
-    // pub payload: Box<Value>,
+    pub payload: Box<Value>,
+
     #[cfg_attr(feature = "utoipa", schema(no_recursion))]
     /// The previous payload, or None if this resource is newly created
-    // theres probably a better way to do this, but its the best solution i could think of for now
-    pub payload_prev: Option<Box<MessageSync>>,
-    // pub payload_prev: Option<Box<Value>>,
+    pub payload_prev: Option<Box<Value>>,
 }
 
 mod next {
