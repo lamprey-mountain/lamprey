5c075d2f5584081ccec05626895241397ad4ff66
tezlm <zestylemonade@celery.eu.org>
2025-08-28 11:35:21 -0700

    refactor(media): mess with thumbnail generation more

--- crate-cdn/src/routes.rs
+++ crate-cdn/src/routes.rs
@@ -31,7 +31,7 @@ async fn get_media(
     State(state): State<AppState>,
     Path(media_id): Path<MediaId>,
     headers: HeaderMap,
-) -> Result<impl IntoResponse> {
+) -> Result<(http::StatusCode, HeaderMap, Body)> {
     let path = format!("/media/{}", media_id);
 
     let media = data::lookup_media(&state.db, media_id).await?;
@@ -174,15 +174,12 @@ fn build_common_headers(req_headers: &HeaderMap, media: &Media) -> Result<Header
     })
 }
 
-// TODO: deduplicate this code
-struct HeaderInfo2 {
+struct ThumbHeaderInfo {
     headers: HeaderMap,
-    range: Option<(Bound<u64>, Bound<u64>)>,
     unmodified: bool,
-    thumb: Option<MediaTrack>,
 }
 
-fn build_common_headers2(req_headers: &HeaderMap, media: &Media, size: u64) -> Result<HeaderInfo2> {
+fn build_thumb_headers_pre(req_headers: &HeaderMap, media: &Media) -> Result<ThumbHeaderInfo> {
     let mut headers = HeaderMap::new();
     headers.typed_insert(headers::AcceptRanges::bytes());
     headers.typed_insert("image/avif".parse::<headers::ContentType>().unwrap());
@@ -214,64 +211,51 @@ fn build_common_headers2(req_headers: &HeaderMap, media: &Media, size: u64) -> R
     let lm = headers::LastModified::from(id_timestamp);
     headers.typed_insert(lm);
 
+    if let Some(if_none_match) = req_headers.typed_get::<headers::IfNoneMatch>() {
+        if !if_none_match.precondition_passes(&etag) {
+            return Ok(ThumbHeaderInfo {
+                headers,
+                unmodified: true,
+            });
+        }
+    }
+
+    if let Some(if_modified_since) = req_headers.typed_get::<headers::IfModifiedSince>() {
+        if !if_modified_since.is_modified(id_timestamp) {
+            return Ok(ThumbHeaderInfo {
+                headers,
+                unmodified: true,
+            });
+        }
+    }
+
+    Ok(ThumbHeaderInfo {
+        headers,
+        unmodified: false,
+    })
+}
+
+fn complete_thumb_headers(
+    req_headers: &HeaderMap,
+    media: &Media,
+    mut headers: HeaderMap,
+    content_length: u64,
+) -> Result<(HeaderMap, Option<(Bound<u64>, Bound<u64>)>)> {
+    let etag: headers::ETag = format!("W/\"{}\"", media.id).parse().unwrap();
+    let id_timestamp: SystemTime = media
+        .id
+        .into_inner()
+        .get_timestamp()
+        .expect("all uuids are uuidv7")
+        .into();
+    let lm = headers::LastModified::from(id_timestamp);
+
     let allow_range_requests = if let Some(if_range) = headers.typed_get::<headers::IfRange>() {
         !if_range.is_modified(Some(&etag), Some(&lm))
     } else {
-        if let Some(if_none_match) = req_headers.typed_get::<headers::IfNoneMatch>() {
-            if !if_none_match.precondition_passes(&etag) {
-                return Ok(HeaderInfo2 {
-                    headers,
-                    range: None,
-                    unmodified: true,
-                    thumb: None,
-                });
-            }
-        }
-
-        if let Some(if_modified_since) = req_headers.typed_get::<headers::IfModifiedSince>() {
-            if !if_modified_since.is_modified(id_timestamp) {
-                return Ok(HeaderInfo2 {
-                    headers,
-                    range: None,
-                    unmodified: true,
-                    thumb: None,
-                });
-            }
-        }
-
         true
     };
 
-    // TODO: remove the following thumb selection code - i don't need it with a cdn!
-    // i can always generate the right size thumbnail. i don't need to check tracks manually
-
-    // direct port of the frontend algorithm: "get the largest image that fits in a w by h rect"
-    // theres almost certainly a better way of doing this
-    let mut thumbs: Vec<_> = media
-        .all_tracks()
-        .filter_map(|t| match &t.info {
-            MediaTrackInfo::Thumbnail(i) => Some((t, i.width, i.height)),
-            MediaTrackInfo::Image(i) => Some((t, i.width, i.height)),
-            MediaTrackInfo::Mixed(i) => match (i.width, i.height) {
-                (Some(w), Some(h)) => Some((t, w, h)),
-                _ => None,
-            },
-            _ => None,
-        })
-        .collect();
-    thumbs.sort_by(|(_, a, _), (_, b, _)| b.cmp(a));
-    let thumb = thumbs
-        .iter()
-        .find_map(|(t, w, h)| {
-            if *w <= size && *h <= size {
-                Some(t)
-            } else {
-                None
-            }
-        })
-        .ok_or(Error::NotFound)?;
-
-    let content_length = thumb.size;
     if allow_range_requests {
         if let Some(ranges) = req_headers.typed_get::<headers::Range>() {
             let ranges: Vec<_> = ranges.satisfiable_ranges(content_length).collect();
@@ -280,21 +264,11 @@ fn build_common_headers2(req_headers: &HeaderMap, media: &Media, size: u64) -> R
             }
             let range = ranges[0];
             headers.typed_insert(headers::ContentRange::bytes(range, content_length).unwrap());
-            return Ok(HeaderInfo2 {
-                headers,
-                range: Some(range),
-                unmodified: false,
-                thumb: Some(thumb.to_owned().to_owned()),
-            });
+            return Ok((headers, Some(range)));
         }
     }
     headers.typed_insert(headers::ContentLength(content_length));
-    Ok(HeaderInfo2 {
-        headers,
-        unmodified: false,
-        range: None,
-        thumb: Some(thumb.to_owned().to_owned()),
-    })
+    Ok((headers, None))
 }
 
 /// get the MediaTrack the thumbnail should be generated from
@@ -308,19 +282,18 @@ fn get_thumb_source(media: &Media) -> Option<&MediaTrack> {
             }
         }
         _ => {
-            if let Some(t) = media
-                .all_tracks()
-                .find(|t| t.source == TrackSource::Extracted && matches!(t.info, MediaTrackInfo::Thumbnail(_))) {
+            if let Some(t) = media.all_tracks().find(|t| {
+                t.source == TrackSource::Extracted && matches!(t.info, MediaTrackInfo::Thumbnail(_))
+            }) {
                 Some(t)
             } else {
-            media.all_tracks().find(|t| match &t.info {
-                MediaTrackInfo::Thumbnail(_) => true,
-                MediaTrackInfo::Image(_) => true,
-                _ => false,
-            })
-
+                media.all_tracks().find(|t| {
+                    matches!(
+                        t.info,
+                        MediaTrackInfo::Thumbnail(_) | MediaTrackInfo::Image(_)
+                    )
+                })
             }
-
         }
     }
 }
@@ -340,39 +313,42 @@ async fn get_thumb(
     Path(media_id): Path<MediaId>,
     Query(query): Query<ThumbQuery>,
     headers: HeaderMap,
-) -> Result<impl IntoResponse> {
-    // TODO: save original thumbs (eg. from videos) to /thumb/{media-id}/original
-    // TODO: return original thumbs if size is None. fallback to original image?
-
-    let size = query.size.unwrap_or(64);
-    if !state.config.thumb_sizes.contains(&size) {
-        return Err(Error::BadRequest);
-    }
-
-    let media = data::lookup_media(&state.db, media_id).await?;
-    let header_info = build_common_headers2(&headers, &media, size as u64)?;
-
-    if header_info.unmodified {
-        return Ok((StatusCode::NOT_MODIFIED, header_info.headers, Body::empty()));
-    }
-
-    let Some(thumb) = header_info.thumb else {
-        debug!("no valid thumbnail for this image");
-        // no valid thumbnail for this image
-        return Ok((StatusCode::NOT_FOUND, header_info.headers, Body::empty()));
-    };
-
-    let thumb_path = thumb.url.path();
-    if state.s3.exists(&thumb_path).await? {
-        let reader = state.s3.reader(&thumb_path).await?;
-        if let Some(r) = header_info.range {
-            let body = Body::from_stream(reader.into_bytes_stream(r).await?);
-            Ok((StatusCode::PARTIAL_CONTENT, header_info.headers, body))
-        } else {
-            let body = Body::from_stream(reader.into_bytes_stream(..).await?);
-            Ok((StatusCode::OK, header_info.headers, body))
+) -> Result<(http::StatusCode, HeaderMap, Body)> {
+    // NOTE: original thumbnails (eg. from videos) are already extracted and saved to /thumb/{media_id}/original
+    if let Some(size) = query.size {
+        if !state.config.thumb_sizes.contains(&size) {
+            return Err(Error::BadRequest);
         }
-    } else {
+
+        let media = data::lookup_media(&state.db, media_id).await?;
+        let pre_header_info = build_thumb_headers_pre(&headers, &media)?;
+
+        if pre_header_info.unmodified {
+            return Ok((
+                StatusCode::NOT_MODIFIED,
+                pre_header_info.headers,
+                Body::empty(),
+            ));
+        }
+
+        let thumb_path = format!("/thumb/{media_id}/{size}x{size}");
+
+        if state.s3.exists(&thumb_path).await? {
+            let meta = state.s3.stat(&thumb_path).await?;
+            let content_length = meta.content_length();
+            let (headers, range) =
+                complete_thumb_headers(&headers, &media, pre_header_info.headers, content_length)?;
+
+            let reader = state.s3.reader(&thumb_path).await?;
+            if let Some(r) = range {
+                let body = Body::from_stream(reader.into_bytes_stream(r).await?);
+                return Ok((StatusCode::PARTIAL_CONTENT, headers, body));
+            } else {
+                let body = Body::from_stream(reader.into_bytes_stream(..).await?);
+                return Ok((StatusCode::OK, headers, body));
+            }
+        }
+
         // TODO: prevent races when generating thumbs
         // let thumb_lock = state
         //     .inflight
@@ -383,9 +359,13 @@ async fn get_thumb(
         // // generate thumbnail...
         // drop(_guard);
 
+        let Some(source_track) = get_thumb_source(&media) else {
+            return Err(Error::NotFound);
+        };
+
         let image_data = state
             .s3
-            .read(media.source.url.path())
+            .read(source_track.url.path())
             .instrument(span!(Level::INFO, "read source media from s3"))
             .await?
             .to_bytes();
@@ -400,12 +380,11 @@ async fn get_thumb(
         .instrument(span!(Level::INFO, "generate thumbnail"))
         .await?;
 
-        let a = thumb_path.to_owned();
-        let b = thumb_data.clone();
         let s = state.s3.clone();
+        let data_clone = thumb_data.clone();
         tokio::spawn(async move {
             if let Err(err) = s
-                .write(&a, b)
+                .write(&thumb_path, data_clone)
                 .instrument(span!(Level::INFO, "upload thumbnail to s3"))
                 .await
             {
@@ -413,7 +392,49 @@ async fn get_thumb(
             }
         });
 
-        Ok((StatusCode::OK, header_info.headers, Body::from(thumb_data)))
+        let (headers, _range) = complete_thumb_headers(
+            &headers,
+            &media,
+            pre_header_info.headers,
+            thumb_data.len() as u64,
+        )?;
+
+        Ok((StatusCode::OK, headers, Body::from(thumb_data)))
+    } else {
+        let media = data::lookup_media(&state.db, media_id).await?;
+        let original_thumb_path = format!("/thumb/{media_id}/original");
+
+        if state.s3.exists(&original_thumb_path).await? {
+            let pre_header_info = build_thumb_headers_pre(&headers, &media)?;
+
+            if pre_header_info.unmodified {
+                return Ok((
+                    StatusCode::NOT_MODIFIED,
+                    pre_header_info.headers,
+                    Body::empty(),
+                ));
+            }
+
+            let meta = state.s3.stat(&original_thumb_path).await?;
+            let content_length = meta.content_length();
+            let (headers, range) =
+                complete_thumb_headers(&headers, &media, pre_header_info.headers, content_length)?;
+
+            let reader = state.s3.reader(&original_thumb_path).await?;
+            if let Some(r) = range {
+                let body = Body::from_stream(reader.into_bytes_stream(r).await?);
+                return Ok((StatusCode::PARTIAL_CONTENT, headers, body));
+            } else {
+                let body = Body::from_stream(reader.into_bytes_stream(..).await?);
+                return Ok((StatusCode::OK, headers, body));
+            }
+        }
+
+        if media.source.mime.starts_with("image/") {
+            return get_media(State(state), Path(media_id), headers).await;
+        }
+
+        Err(Error::NotFound)
     }
 }
 
