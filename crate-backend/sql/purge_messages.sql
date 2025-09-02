78e3b0fac42f5697e6090224b14c286ae92d52a3
tezlm <zestylemonade@celery.eu.org>
2025-09-02 03:34:21 -0700

    fix: delete embed media links in message purge

--- crate-backend/sql/purge_messages.sql
+++ crate-backend/sql/purge_messages.sql
@@ -13,6 +13,7 @@ update media set deleted_at = now() where id in (
 delete from message_attachment where version_id in (select version_id from to_delete);
 delete from media_link where target_id in (select id from to_delete) and link_type = 'Message';
 delete from media_link where target_id in (select version_id from to_delete) and link_type = 'MessageVersion';
+delete from media_link where target_id in (select id from to_delete) and link_type = 'Embed';
 delete from message where version_id in (select version_id from to_delete);
 delete from reaction where message_id in (select version_id from to_delete);
 
