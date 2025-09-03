c15a73ccffa53bdb33266f2537afdd928c33e709
tezlm <zestylemonade@celery.eu.org>
2025-09-03 02:03:26 -0700

    fix(sfu): ip addr selection logic

--- crate-sfu/src/util.rs
+++ crate-sfu/src/util.rs
@@ -1,13 +1,16 @@
 use std::net::IpAddr;
 
 use systemstat::{Platform, System};
+use tracing::debug;
 
 pub fn select_host_address_ipv4(host_ip: Option<&str>) -> IpAddr {
     if let Some(ip) = host_ip {
         if let Ok(addr) = ip.parse() {
+            debug!("using configured ipv4 addr {addr}");
             return addr;
         }
     }
+
     let system = System::new();
     let networks = system.networks().unwrap();
 
@@ -15,6 +18,7 @@ pub fn select_host_address_ipv4(host_ip: Option<&str>) -> IpAddr {
         for n in &net.addrs {
             if let systemstat::IpAddr::V4(v) = n.addr {
                 if !v.is_loopback() && !v.is_link_local() && !v.is_broadcast() && !v.is_private() {
+                    debug!("selected ipv4 addr {v}");
                     return IpAddr::V4(v);
                 }
             }
@@ -27,6 +31,7 @@ pub fn select_host_address_ipv4(host_ip: Option<&str>) -> IpAddr {
 pub fn select_host_address_ipv6(host_ip: Option<&str>) -> IpAddr {
     if let Some(ip) = host_ip {
         if let Ok(addr) = ip.parse() {
+            debug!("using configured ipv6 addr {addr}");
             return addr;
         }
     }
@@ -36,7 +41,12 @@ pub fn select_host_address_ipv6(host_ip: Option<&str>) -> IpAddr {
     for net in networks.values() {
         for n in &net.addrs {
             if let systemstat::IpAddr::V6(v) = n.addr {
-                if !v.is_loopback() && !v.is_unicast_link_local() && !v.is_multicast() {
+                if !v.is_loopback()
+                    && !v.is_unicast_link_local()
+                    && !v.is_multicast()
+                    && !v.is_unique_local()
+                {
+                    debug!("selected ipv6 addr {v}");
                     return IpAddr::V6(v);
                 }
             }
