6eb55678ee64ddc9644752596db544d76d56a87f
tezlm <zestylemonade@celery.eu.org>
2025-10-08 17:31:13 -0700

    fix(api): bad migration

--- crate-backend/migrations/0206_embed_hosts_fn_and_idx.sql
+++ crate-backend/migrations/0206_embed_hosts_fn_and_idx.sql
@@ -18,6 +18,3 @@ BEGIN
     RETURN hosts;
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
-
-CREATE INDEX message_embed_hosts_gin_idx ON message USING GIN (embed_hosts(embeds));
-CREATE INDEX message_pinned_idx ON message (id) WHERE pinned IS NOT NULL and is_latest and deleted_at is null;
