2917942542cae2ed0dee02b44ae2c6eca95c0148
tezlm <noreply@celery.eu.org>
2025-11-30 12:19:53 -0800

    feat(api): sketch out more automod stuff
    Signed-off-by: tezlm <noreply@celery.eu.org>

--- crate-common/src/v1/types/automod.rs
+++ crate-common/src/v1/types/automod.rs
@@ -1,8 +1,11 @@
-use crate::v1::types::{reaction::ReactionKey, AutomodRuleId, ChannelId, RoleId, RoomId};
+use crate::v1::types::{AutomodRuleId, ChannelId, MessageId, RoleId, RoomId, UserId};
+
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
+
 #[cfg(feature = "utoipa")]
 use utoipa::ToSchema;
+
 #[cfg(feature = "validator")]
 use validator::Validate;
 
@@ -16,11 +19,14 @@ pub struct AutomodRule {
     #[schema(max_length = 64)]
     pub name: String,
     pub enabled: bool,
+    // TODO: support multiple triggers
     pub trigger: AutomodTrigger,
     #[schema(max_items = 8)]
     pub actions: Vec<AutomodAction>,
     pub except_roles: Vec<RoleId>,
-    pub except_threads: Vec<ChannelId>,
+    pub except_channels: Vec<ChannelId>,
+    // /// whether this rule should affect everyone. actions aren't necessarily executed (eg. admins wont be timed out)
+    // pub include_everyone: bool,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -35,7 +41,7 @@ pub struct AutomodRuleCreate {
     #[serde(default)]
     pub except_roles: Vec<RoleId>,
     #[serde(default)]
-    pub except_threads: Vec<ChannelId>,
+    pub except_channels: Vec<ChannelId>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -49,12 +55,36 @@ pub struct AutomodRuleUpdate {
     #[cfg_attr(feature = "validator", validate(length(min = 1, max = 8)))]
     pub actions: Option<Vec<AutomodAction>>,
     pub except_roles: Option<Vec<RoleId>>,
-    pub except_threads: Option<Vec<ChannelId>>,
+    pub except_channels: Option<Vec<ChannelId>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[cfg_attr(feature = "utoipa", derive(ToSchema))]
+#[cfg_attr(feature = "validator", derive(Validate))]
+pub struct AutomodRuleExecution {
+    /// the rule that was executed
+    pub rule: AutomodRule,
+
+    /// the user who triggered this rule
+    pub user_id: UserId,
+
+    /// the channel this happened in (for messages)
+    pub channel_id: Option<ChannelId>,
+
+    /// the message this matched (excluded for Block)
+    pub message_id: Option<MessageId>,
+
+    /// the content that was matched against (eg. message content)
+    pub content: String,
+
+    /// the keyword or regex that was matched in the content
+    pub matched: Option<String>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 #[serde(tag = "type")]
 pub enum AutomodTrigger {
+    /// scan a message based on regex
     MessageRegex {
         // max length 32
         deny: Vec<String>,
@@ -63,29 +93,38 @@ pub enum AutomodTrigger {
         allow: Vec<String>,
     },
 
+    /// scan a message based on its keywords. automatically adds word boundaries and decancers the string (ie. properly handles unicode lookalikes).
     MessageKeywords {
         // max length 32
-        words: Vec<String>,
+        keywords: Vec<String>,
     },
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 #[serde(tag = "type")]
 pub enum AutomodAction {
+    /// block the message from being sent
     Block,
 
+    /// timeout a user
     Timeout {
         /// in milliseconds
         duration: u64,
     },
 
+    /// remove a message. unlike Block, remove messages can be allowed/restored by a moderator.
     Remove,
 
+    /// send an alert to a channel
     SendAlert {
-        thread_id: ChannelId,
+        /// where to send the alert to
+        channel_id: ChannelId,
     },
-
-    React {
-        reaction: ReactionKey,
-    },
+    // TODO: automatic reactions?
+    // /// add a reaction to the message
+    // React {
+    //     /// the reaction to add
+    //     // TODO: use ReactionKeyParam here? or at least for patching
+    //     reaction: ReactionKey,
+    // },
 }
