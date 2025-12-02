db9651db021209b113f20721a42d4423c482a4f5
tezlm <noreply@celery.eu.org>
2025-12-02 12:22:45 -0800

    refactor(api): remove useless (and misleading!) function
    Signed-off-by: tezlm <noreply@celery.eu.org>

--- crate-backend/src/types/permission.rs
+++ crate-backend/src/types/permission.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 use std::iter::FromIterator;
 
-use common::v1::types::{defaults::ADMIN_ROOM, Permission, PermissionOverwrite};
+use common::v1::types::{defaults::ADMIN_ROOM, Permission};
 
 use crate::error::{Error, Result};
 
@@ -63,16 +63,6 @@ impl Permissions {
             }
         }
         self.p = new;
-    }
-
-    pub fn apply_overwrite(&mut self, ow: &PermissionOverwrite) {
-        for p in &ow.allow {
-            self.p.insert(*p);
-        }
-
-        for p in &ow.deny {
-            self.p.remove(p);
-        }
     }
 }
 
