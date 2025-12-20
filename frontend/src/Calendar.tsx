1adbb14d6a929546cac0fb8963d3c57ec26414dc
tezlm <noreply@celery.eu.org>
2025-12-20 12:14:04 -0800

    feat(ui): calendar filters
    Signed-off-by: tezlm <noreply@celery.eu.org>

--- frontend/src/Calendar.tsx
+++ frontend/src/Calendar.tsx
@@ -1,3 +1,6 @@
+import { Channel } from "sdk";
+import { createSignal, Match, Switch } from "solid-js";
+
 export const Calendar = (props: { channel: Channel }) => {
 	// TODO: load calendar events from api
 	// TODO: update calendar events from sync
@@ -15,12 +18,13 @@ export const Calendar = (props: { channel: Channel }) => {
 	// TODO: rsvp/unrsvp to events
 	// TODO: list rsvps for events
 
-	const offset = -1;
-	const today = 9;
 	const events = new Map([
 		[12, ["foo", "bar"]],
 		[16, ["baz"]],
 	]);
+
+	const [view, setView] = createSignal<"week" | "month" | "timeline">("month");
+
 	return (
 		<div class="calendar">
 			<header>
@@ -28,36 +32,83 @@ export const Calendar = (props: { channel: Channel }) => {
 				<div style="flex:1"></div>
 				<menu>
 					<div class="filters">
-						<button>week</button>
-						<button>month</button>
-						<button>timeline</button>
+						<button
+							onClick={() => setView("week")}
+							classList={{ active: view() === "week" }}
+						>
+							week
+						</button>
+						<button
+							onClick={() => setView("month")}
+							classList={{ active: view() === "month" }}
+						>
+							month
+						</button>
+						<button
+							onClick={() => setView("timeline")}
+							classList={{ active: view() === "timeline" }}
+						>
+							timeline
+						</button>
 					</div>
-					<button style="margin-left:4px" class="primary">today</button>
+					<div class="filters" style="margin-left:4px">
+						<button>prev</button>
+						<button>next</button>
+						<button class="primary">today</button>
+					</div>
 				</menu>
 			</header>
-			<div class="month-view">
-				{["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].map((i) => (
-					<div class="dayofweek">{i}</div>
-				))}
-				{new Array(28).fill(0).map((_, i) => {
-					const day0 = i + offset;
-					const day = day0 > 0 ? day0 : day0 + 31;
-					return (
-						<div
-							class="day"
-							classList={{
-								othermonth: day0 <= 0,
-								today: day0 === today,
-							}}
-						>
-							<span class="daynumber">{day}</span>
-							{(events.get(day) ?? []).map((event) => (
-								<span class="event">{event}</span>
-							))}
-						</div>
-					);
-				})}
-			</div>
+			<Switch>
+				<Match when={view() === "week"}>
+					<CalendarWeek channel={props.channel} events={events} />
+				</Match>
+				<Match when={view() === "month"}>
+					<CalendarMonth channel={props.channel} events={events} />
+				</Match>
+				<Match when={view() === "timeline"}>
+					<CalendarTimeline channel={props.channel} events={events} />
+				</Match>
+			</Switch>
 		</div>
 	);
+};
+
+// TODO: strongly type events
+const CalendarMonth = (props: { channel: Channel; events: any }) => {
+	const offset = -1;
+	const today = 9;
+
+	return (
+		<div class="month-view">
+			{["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].map((i) => (
+				<div class="dayofweek">{i}</div>
+			))}
+			{new Array(28).fill(0).map((_, i) => {
+				const day0 = i + offset;
+				const day = day0 > 0 ? day0 : day0 + 31;
+				return (
+					<div
+						class="day"
+						classList={{
+							othermonth: day0 <= 0,
+							today: day0 === today,
+						}}
+					>
+						<span class="daynumber">{day}</span>
+						{(props.events.get(day) ?? []).map((event) => (
+							<span class="event">{event}</span>
+						))}
+					</div>
+				);
+			})}
+		</div>
+	);
+};
+
+const CalendarWeek = (props: { channel: Channel; events: any }) => {
+	return "todo";
+};
+
+const CalendarTimeline = (props: { channel: Channel; events: any }) => {
+	return "todo";
 };
