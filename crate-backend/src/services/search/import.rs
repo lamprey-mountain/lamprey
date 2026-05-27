6d55798b300dadb763b62b8ad395f2baa502844d
tezlm <noreply@celery.eu.org>
2026-05-27 07:42:00 -0700

    feat(api): handle room/user deletes
    Signed-off-by: tezlm <noreply@celery.eu.org>

--- crate-backend/src/services/search/import.rs
+++ crate-backend/src/services/search/import.rs
@@ -112,6 +112,20 @@ impl ContentIngestionManager {
                                 MessageSync::UserUpdate { user } => {
                                     self.index_user(user, true).await;
                                 }
+                                MessageSync::RoomDelete { room_id } => {
+                                    let term = Term::from_field_text(
+                                        self.schema.id,
+                                        &room_id.to_string(),
+                                    );
+                                    let _ = self.index_writer.tell(DeleteTerm(term)).await;
+                                }
+                                MessageSync::UserDelete { id } => {
+                                    let term = Term::from_field_text(
+                                        self.schema.id,
+                                        &id.to_string(),
+                                    );
+                                    let _ = self.index_writer.tell(DeleteTerm(term)).await;
+                                }
                                 MessageSync::MediaProcessed { media, .. } => {
                                     self.index_media(media, false).await;
                                 }
