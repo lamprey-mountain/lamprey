bf71b4720d146deb7a5792d051b823a121da1c9c
tezlm <noreply@celery.eu.org>
2026-06-19 08:19:08 -0700

    feat: handle message mentions
    Signed-off-by: tezlm <noreply@celery.eu.org>

--- crate-backend/src/services/notifications/calculator.rs
+++ crate-backend/src/services/notifications/calculator.rs
@@ -1,15 +1,114 @@
-use crate::{prelude::*, services::notifications::ServiceNotifications};
-use common::v1::types::{
-    Channel, ChannelId, Message, Room, RoomId, UserId,
-    notifications::{
-        Notification,
-        preferences::{
-            Mute, NotifsChannel, NotifsGlobal, NotifsMessages, NotifsReplies, NotifsRoom,
-            NotifsThreads,
+use crate::{prelude::*, services::notifications::MentionedUsers};
+use common::{
+    v1::types::{
+        Channel, ChannelId, Message, Room, RoomId, UserId,
+        notifications::{
+            Notification, NotificationType,
+            preferences::{
+                Mute, NotifsChannel, NotifsGlobal, NotifsMessages, NotifsReactions, NotifsReplies,
+                NotifsRoom, NotifsThreads,
+            },
         },
+        util::Time,
     },
-    util::Time,
+    v2::types::NotificationId,
 };
+
+/// actions to take on this event
+pub struct Actions {
+    should_push: bool,
+    should_add_to_inbox: bool,
+    should_increment_mention_count: bool,
+    notification: Option<Notification>,
+}
+
+/// What action to take for a notification
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum NotificationAction {
+    /// Don't do anything
+    Skip,
+
+    /// Add to inbox only, no push notification
+    Inbox,
+
+    /// Send push notification and add to inbox
+    Push,
+}
+
+impl Actions {
+    /// Whether this notification should be sent as a push notification
+    pub fn should_push(&self) -> bool {
+        self.should_push
+    }
+
+    /// Whether this notification should be added to the inbox
+    pub fn should_add_to_inbox(&self) -> bool {
+        self.should_add_to_inbox
+    }
+
+    /// Whether the mention count should be incremented
+    pub fn should_increment_mention_count(&self) -> bool {
+        self.should_increment_mention_count
+    }
+
+    /// Get the notification that should be created for this user
+    pub fn notification(&self) -> Option<&Notification> {
+        self.notification.as_ref()
+    }
+}
+
+impl NotificationAction {
+    /// Merge these actions with other actions
+    pub fn merge(&self, other: Self) -> Self {
+        use NotificationAction::*;
+        match (*self, other) {
+            (Push, _) | (_, Push) => Push,
+            (Inbox, _) | (_, Inbox) => Inbox,
+            (Skip, Skip) => Skip,
+        }
+    }
+
+    /// Whether this notification should be sent as a push notification
+    pub fn should_push(&self) -> bool {
+        matches!(self, NotificationAction::Push)
+    }
+
+    /// Whether this notification should be added to the inbox
+    pub fn should_add_to_inbox(&self) -> bool {
+        matches!(self, NotificationAction::Inbox | NotificationAction::Push)
+    }
+}
+
+impl From<NotifsMessages> for NotificationAction {
+    fn from(value: NotifsMessages) -> Self {
+        match value {
+            NotifsMessages::Nothing => NotificationAction::Skip,
+            NotifsMessages::Mentions => NotificationAction::Inbox,
+            NotifsMessages::Watching => NotificationAction::Inbox,
+            NotifsMessages::Everything => NotificationAction::Push,
+        }
+    }
+}
+
+impl From<NotifsReplies> for NotificationAction {
+    fn from(value: NotifsReplies) -> Self {
+        match value {
+            NotifsReplies::Notify => NotificationAction::Push,
+            NotifsReplies::Watching => NotificationAction::Inbox,
+            NotifsReplies::Nothing => NotificationAction::Skip,
+        }
+    }
+}
+
+impl From<NotifsThreads> for NotificationAction {
+    fn from(value: NotifsThreads) -> Self {
+        match value {
+            NotifsThreads::Notify => NotificationAction::Push,
+            NotifsThreads::Inbox => NotificationAction::Inbox,
+            NotifsThreads::Nothing => NotificationAction::Skip,
+        }
+    }
+}
 
 /// a set of notification preferences for a user
 pub struct Preferences {
@@ -20,40 +119,288 @@ pub struct Preferences {
 
 /// notification calculator
 pub struct Calculator {
-    _state: Globals,
+    globals: Globals,
 
     // context
-    _room: Option<Room>,
-    _channel: Option<Channel>,
-    _message: Option<Message>,
-    // notification: Option<Notification>,
-    // TODO
-}
-
-/// actions to take on this event
-pub struct Actions {
-    // TODO
+    room: Option<Room>,
+    channel: Option<Channel>,
+    replied_message: Option<Message>,
+    message: Option<Message>,
+    mentioned_users: Option<MentionedUsers>,
+    notification: Option<Notification>,
 }
 
 impl Calculator {
-    // pub async fn load_for_message(state: ServerState2, message: &Message) -> Result<Self> {
     pub async fn load_for_message(
-        _state: Globals,
-        _channel: &Channel,
-        _message: &Message,
+        globals: Globals,
+        channel: &Channel,
+        message: &Message,
     ) -> Result<Self> {
-        todo!()
+        let srv = globals.services();
+
+        let replied_message = if let Some(reply_id) = message.latest_version.reply_id {
+            srv.messages.get(channel.id, reply_id, None).await.ok()
+        } else {
+            None
+        };
+
+        let room = if let Some(room_id) = channel.room_id {
+            srv.rooms.get(room_id, None).await.ok()
+        } else {
+            None
+        };
+
+        let mentioned_users = srv
+            .notifications
+            .get_mentioned_users(channel, message)
+            .await
+            .ok();
+
+        Ok(Self {
+            globals,
+            room,
+            channel: Some(channel.clone()),
+            replied_message,
+            message: Some(message.clone()),
+            mentioned_users,
+            notification: None,
+        })
     }
 
-    pub async fn load_for_notification(_state: Globals, _notif: &Notification) -> Result<Self> {
-        todo!()
+    pub async fn load_for_notification(globals: Globals, notif: &Notification) -> Result<Self> {
+        let srv = globals.services();
+
+        let channel_id = notif.channel_id();
+        let channel = if let Some(id) = channel_id {
+            srv.channels.get(id, None).await.ok()
+        } else {
+            None
+        };
+
+        let room_id = channel.as_ref().and_then(|ch| ch.room_id);
+        let room = if let Some(id) = room_id {
+            srv.rooms.get(id, None).await.ok()
+        } else {
+            None
+        };
+
+        Ok(Self {
+            globals,
+            room,
+            channel,
+            replied_message: None,
+            message: None,
+            mentioned_users: None,
+            notification: Some(notif.clone()),
+        })
     }
 
     /// calculate notification actions for a user
-    pub async fn calculate(&self, _user_id: UserId) -> Result<Actions> {
-        // TODO: return a Notification/NotificationType?
-        todo!()
+    // TODO: drop notification if message author is ignored or blocked
+    pub async fn calculate(&self, user_id: UserId) -> Result<Actions> {
+        let room_id = self.channel.as_ref().and_then(|c| c.room_id);
+        let channel_id = self.channel.as_ref().map(|c| c.id);
+        let prefs = Preferences::load(&self.globals, user_id, room_id, channel_id).await?;
+
+        // NOTE: maybe make this an enum
+        let (notif, action) = if let Some(message) = &self.message {
+            let channel = self.channel.as_ref().ok_or_else(|| {
+                Error::Internal("missing channel for message notification".to_string())
+            })?;
+
+            let mention_user = self
+                .mentioned_users
+                .as_ref()
+                .map_or(false, |m| m.users_from_direct.contains(&user_id));
+            let mention_everyone = self
+                .mentioned_users
+                .as_ref()
+                .map_or(false, |m| m.users_from_everyone.contains(&user_id));
+            let mention_role = self
+                .mentioned_users
+                .as_ref()
+                .map_or(false, |m| m.users_from_role.contains(&user_id));
+            let reply = self
+                .replied_message
+                .as_ref()
+                .map_or(false, |m| m.author_id == user_id);
+
+            let notif = Notification {
+                id: NotificationId::new(),
+                ty: NotificationType::Message {
+                    room_id: channel.room_id,
+                    channel_id: channel.id,
+                    message_id: message.id,
+                    user_id: message.author_id,
+                    mention_user,
+                    mention_everyone,
+                    mention_role,
+                    reply,
+                },
+                added_at: Time::now_utc(),
+                read_at: None,
+                note: None,
+            };
+
+            let action = self.calculate_message_action(
+                &prefs,
+                mention_user,
+                mention_everyone,
+                mention_role,
+                reply,
+            );
+            (Some(notif), action)
+        } else if let Some(notif) = &self.notification {
+            let action = self.calculate_notification_action(&prefs, notif);
+            (Some(notif.clone()), action)
+        } else {
+            return Err(Error::Internal(
+                "no message or notification context in calculator".to_string(),
+            ));
+        };
+
+        // Determine if the user was actively mentioned after applying room rules
+        let mentioned = if let Some(n) = &notif {
+            if let NotificationType::Message {
+                mention_user,
+                mention_everyone,
+                mention_role,
+                ..
+            } = &n.ty
+            {
+                let room_prefs = prefs.room.as_ref();
+                let mention_role_allowed = if let Some(rp) = room_prefs {
+                    *mention_role && rp.mention_roles
+                } else {
+                    *mention_role
+                };
+                let mention_everyone_allowed = if let Some(rp) = room_prefs {
+                    *mention_everyone && rp.mention_everyone
+                } else {
+                    *mention_everyone
+                };
+                *mention_user || mention_role_allowed || mention_everyone_allowed
+            } else {
+                false
+            }
+        } else {
+            false
+        };
+
+        let is_dm = self.channel.as_ref().map_or(false, |c| c.ty.is_dm());
+        let should_increment_mention_count = !prefs.is_muted() && (mentioned || is_dm);
+
+        Ok(Actions {
+            should_push: action.should_push(),
+            should_add_to_inbox: action.should_add_to_inbox(),
+            should_increment_mention_count,
+            notification: notif,
+        })
     }
+
+    fn calculate_message_action(
+        &self,
+        prefs: &Preferences,
+        mention_user: bool,
+        mention_everyone: bool,
+        mention_role: bool,
+        reply: bool,
+    ) -> NotificationAction {
+        if prefs.is_muted() {
+            return NotificationAction::Skip;
+        }
+
+        let reply_action = if reply {
+            prefs.resolve_replies().clone().into()
+        } else {
+            NotificationAction::Skip
+        };
+
+        let mentioned = if let Some(room_prefs) = &prefs.room {
+            let mention_role_allowed = mention_role && room_prefs.mention_roles;
+            let mention_everyone_allowed = mention_everyone && room_prefs.mention_everyone;
+            mention_user || mention_role_allowed || mention_everyone_allowed
+        } else {
+            mention_user || mention_role || mention_everyone
+        };
+
+        let msg_pref = prefs.resolve_messages();
+        let message_action = match msg_pref {
+            NotifsMessages::Everything => NotificationAction::Push,
+            NotifsMessages::Watching => {
+                if mentioned {
+                    NotificationAction::Push
+                } else {
+                    NotificationAction::Inbox
+                }
+            }
+            NotifsMessages::Mentions => {
+                if mentioned {
+                    NotificationAction::Push
+                } else {
+                    NotificationAction::Skip
+                }
+            }
+            NotifsMessages::Nothing => NotificationAction::Skip,
+        };
+
+        reply_action.merge(message_action)
+    }
+
+    fn calculate_notification_action(
+        &self,
+        prefs: &Preferences,
+        notif: &Notification,
+    ) -> NotificationAction {
+        if prefs.is_muted() {
+            return match notif.ty {
+                NotificationType::FriendRequestSent { .. }
+                | NotificationType::FriendRequestReceived { .. }
+                | NotificationType::FriendRequestAccepted { .. } => NotificationAction::Inbox,
+                _ => NotificationAction::Skip,
+            };
+        }
+
+        match &notif.ty {
+            NotificationType::Message {
+                mention_user,
+                mention_everyone,
+                mention_role,
+                reply,
+                ..
+            } => self.calculate_message_action(
+                prefs,
+                *mention_user,
+                *mention_everyone,
+                *mention_role,
+                *reply,
+            ),
+            NotificationType::Thread { .. } => prefs.resolve_threads().clone().into(),
+            NotificationType::Reaction { .. } => match prefs.global.reactions {
+                NotifsReactions::Always => NotificationAction::Push,
+                // FIXME: NotifsReactions::Restricted should be enabled for private rooms?
+                // i may remove Restricted soon
+                NotifsReactions::Restricted | NotifsReactions::Dms => {
+                    if let Some(chan) = &self.channel {
+                        if chan.ty.is_dm() {
+                            NotificationAction::Push
+                        } else {
+                            NotificationAction::Skip
+                        }
+                    } else {
+                        NotificationAction::Skip
+                    }
+                }
+                NotifsReactions::Nothing => NotificationAction::Skip,
+            },
+            NotificationType::FriendRequestSent { .. }
+            | NotificationType::FriendRequestReceived { .. }
+            | NotificationType::FriendRequestAccepted { .. } => NotificationAction::Push,
+        }
+    }
+
+    // TODO: fn room_id(&self) -> Option<RoomId>
+    // TODO: fn channel_id(&self) -> Option<ChannelId>
 }
 
 impl Preferences {
@@ -147,32 +494,4 @@ impl Preferences {
             .or_else(|| self.room.as_ref().and_then(|r| r.threads.as_ref()))
             .unwrap_or(&self.global.threads)
     }
-}
-
-impl Actions {
-    /// Whether this notification should be sent as a push notification
-    pub fn should_push(&self) -> bool {
-        todo!()
-    }
-
-    /// Whether this notification should be added to the inbox
-    pub fn should_add_to_inbox(&self) -> bool {
-        todo!()
-    }
-
-    /// Whether the mention count should be incremented
-    pub fn should_increment_mention_count(&self) -> bool {
-        todo!()
-    }
-}
-
-impl ServiceNotifications {
-    // /// calculate the actions to take for a notification
-    // pub async fn calculate_actions(
-    //     &self,
-    //     user_id: UserId,
-    //     notif: &Notification,
-    // ) -> Result<NotificationAction> {
-    //     calculate(&self.state, user_id, notif).await
-    // }
 }
--- crate-backend/src/services/notifications/mod.rs
+++ crate-backend/src/services/notifications/mod.rs
@@ -1,16 +1,16 @@
 use std::collections::HashSet;
 
 use common::v1::types::notifications::Notification;
-use common::v1::types::{Channel, Message, UserId};
+use common::v1::types::{Channel, Message, MessageSync, UserId};
 use lamprey_backend_data_postgres::MAX_ROLE_MENTION_MEMBERS;
 use tokio::sync::RwLock;
+use tracing::warn;
 
 use crate::prelude::*;
 use crate::services::notifications::push::VapidKeys;
 
 pub mod ack;
 pub mod calculator;
-pub mod calculator_old;
 pub mod push;
 
 pub struct ServiceNotifications {
@@ -19,18 +19,29 @@ pub struct ServiceNotifications {
 }
 
 #[derive(Debug, Default)]
-struct MentionedUsers {
+pub struct MentionedUsers {
     /// users who were directly mentioned
-    users_from_direct: HashSet<UserId>,
+    pub users_from_direct: HashSet<UserId>,
 
     /// users who were mentioned from a role mention
-    users_from_role: HashSet<UserId>,
+    pub users_from_role: HashSet<UserId>,
 
     /// users who were mentioned from an everyone mention
-    users_from_everyone: HashSet<UserId>,
+    pub users_from_everyone: HashSet<UserId>,
 
     /// users who were mentioned due to being a channel recipient
-    users_from_recipient: HashSet<UserId>,
+    pub users_from_recipient: HashSet<UserId>,
+}
+
+impl MentionedUsers {
+    pub fn all(&self) -> HashSet<UserId> {
+        let mut all = HashSet::new();
+        all.extend(&self.users_from_direct);
+        all.extend(&self.users_from_role);
+        all.extend(&self.users_from_everyone);
+        all.extend(&self.users_from_recipient);
+        all
+    }
 }
 
 impl ServiceNotifications {
@@ -48,27 +59,118 @@ impl ServiceNotifications {
     // TODO: flush ack states on shutdown
 
     // NOTE: should ServiceNotifications *really* be in charge of inserting thread members?
+    // TODO: fn process_message_inner -> Result, make process_message do logging
     pub async fn process_message(&self, channel: Channel, message: Message) {
         //  ephemeral messages dont create notifications (or insert thread members)
+        // TODO: move this logic into calculator
         if message.ephemeral {
             return;
         }
 
-        let _mentioned_users = self
+        let calc =
+            match calculator::Calculator::load_for_message(self.state.clone(), &channel, &message)
+                .await
+            {
+                Ok(c) => c,
+                Err(err) => {
+                    warn!("failed to load calculator: {err:?}");
+                    return;
+                }
+            };
+
+        // PERF: don't get_mentioned_users twice (Calculator::load_for_message also calls this)
+        let mentioned_users = self
             .get_mentioned_users(&channel, &message)
             .await
-            .expect("TODO: better error handling");
+            .unwrap_or_default(); // TODO: better error logging
 
-        // insert thread members
-        // PERF: data.thread_member_bulk_insert?
-        // emit sync events
+        let targets: Vec<UserId> = mentioned_users
+            .all()
+            .into_iter()
+            .filter(|&id| id != message.author_id) // TODO: move this logic into calculator
+            .collect();
+        if targets.is_empty() {
+            return;
+        }
 
-        // update ack states (increment mention counts)
-        // emit sync events
+        let mut data = match self.state.begin().await {
+            Ok(d) => d,
+            Err(err) => {
+                warn!("failed to begin database transaction, skipping: {err:?}");
+                return;
+            }
+        };
 
-        // send notifications
+        let mut users_to_increment = vec![];
+        for target in &targets {
+            let action = match calc.calculate(*target).await {
+                Ok(a) => a,
+                Err(err) => {
+                    warn!("failed to calculate actions for user {target:?}: {err:?}");
+                    continue;
+                }
+            };
 
-        todo!()
+            if action.should_increment_mention_count() {
+                users_to_increment.push(*target);
+            }
+
+            if action.should_add_to_inbox() {
+                if let Some(notification) = action.notification() {
+                    if let Err(err) = data.notification_add(*target, notification.clone()).await {
+                        warn!("failed to add notification: {err:?}");
+                    }
+                }
+            }
+
+            // FIXME: handle action.should_push()
+            // TODO: add action.should_add_to_thread()
+        }
+
+        let mut thread_members = vec![];
+        if channel.ty.is_thread() {
+            let _ = data.thread_member_put_bulk(channel.id, &targets).await;
+            thread_members = data
+                .thread_member_get_many(channel.id, &targets)
+                .await
+                .unwrap_or_default();
+        }
+
+        if !users_to_increment.is_empty() {
+            if let Err(err) = data
+                .unread_increment_counts(channel.id, &users_to_increment, &[])
+                .await
+            {
+                warn!("failed to increment unread counts: {err:?}");
+            }
+        }
+
+        if let Err(err) = data.commit().await {
+            warn!("failed to commit database transaction: {err:?}");
+        }
+
+        let srv = self.state.services();
+        if !thread_members.is_empty() {
+            let thread_id = channel.id;
+
+            srv.channels.invalidate(thread_id).await;
+
+            let msg = MessageSync::ThreadMemberUpsert {
+                room_id: channel.room_id,
+                thread_id,
+                added: thread_members,
+                removed: vec![],
+            };
+
+            if let Err(err) = self
+                .state
+                .messaging()
+                .broadcast_channel(thread_id, msg)
+                .await
+            {
+                warn!("failed to broadcast thread member update: {err:?}");
+            }
+        }
     }
 
     /// get a set of ids of all users who were mentioned in a message
