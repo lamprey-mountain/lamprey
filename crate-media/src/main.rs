d5a32b77677105e357aaea9c22bfb801da371847
tezlm <noreply@celery.eu.org>
2026-07-04 13:20:24 -0700

    fix(media): tracing subscriber, otel, load .env file
    Signed-off-by: tezlm <noreply@celery.eu.org>

--- crate-media/src/main.rs
+++ crate-media/src/main.rs
@@ -2,15 +2,22 @@ use figment::{
     Figment,
     providers::{Env, Format, Toml},
 };
-use lamprey_media::{config::Config, server::MediaServer};
+use lamprey_media::{
+    config::Config,
+    server::{MediaServer, setup_otel},
+};
 use tracing::info;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let _ = dotenvy::dotenv();
+
     let config: Config = Figment::new()
         .merge(Toml::file("cdn.toml"))
         .merge(Env::raw().only(&["RUST_LOG"]))
         .extract()?;
+
+    setup_otel(&config)?;
 
     info!("starting cdn with config: {:#?}", config);
 
