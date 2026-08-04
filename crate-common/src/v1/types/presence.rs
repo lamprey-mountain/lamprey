add4279c0e9158893af70ec47ab83e1d6375ecad
tezlm <noreply@celery.eu.org>
2026-08-04 00:51:21 -0700

    refactor: use #[record] for user presence types
    Signed-off-by: tezlm <noreply@celery.eu.org>

--- crate-common/src/v1/types/presence.rs
+++ crate-common/src/v1/types/presence.rs
@@ -1,31 +1,21 @@
-#[cfg(feature = "serde")]
-use serde::{Deserialize, Serialize};
+use lamprey_macros::record;
 
 use crate::v1::types::util::Time;
 
-#[cfg(feature = "utoipa")]
-use utoipa::ToSchema;
-
-#[cfg(feature = "validator")]
-use validator::Validate;
-
 /// the current presence of the user
-#[derive(Debug, Clone, PartialEq, Eq)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(feature = "utoipa", derive(ToSchema))]
-#[cfg_attr(feature = "validator", derive(Validate))]
+#[record]
+#[derive(PartialEq, Eq)]
 pub struct Presence {
     pub status: Status,
 
-    #[cfg_attr(feature = "utoipa", schema(max_length = 8))]
-    #[cfg_attr(feature = "validator", validate(length(max = 8)))]
+    #[schema(max_length = 8)]
+    #[validate(length(max = 8))]
     pub activities: Vec<Activity>,
 }
 
 // NOTE: do i want to rename away/busy to idle/dnd? (or away -> afk?)
-#[derive(Debug, Default, Clone, PartialEq, Eq)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(feature = "utoipa", derive(ToSchema))]
+#[record]
+#[derive(Default, Copy, PartialEq, Eq)]
 pub enum Status {
     /// offline or explicitly invisible
     #[default]
@@ -44,17 +34,17 @@ pub enum Status {
     Available,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(feature = "utoipa", derive(ToSchema))]
-#[cfg_attr(feature = "serde", serde(tag = "type"))]
+#[record]
+#[serde(tag = "type")]
+#[derive(PartialEq, Eq)]
 pub enum Activity {
     /// custom activity
     Custom {
-        #[cfg_attr(feature = "utoipa", schema(max_length = 256))]
+        #[schema(max_length = 256)]
         text: String,
         clear_at: Option<Time>,
     },
+    // TODO: add more activity types
 }
 
 impl Status {
@@ -89,7 +79,7 @@ impl Presence {
 }
 
 #[cfg(feature = "validator")]
-mod validate {
+mod _v {
     use serde_json::json;
     use validator::{Validate, ValidateLength, ValidationError, ValidationErrors};
 
