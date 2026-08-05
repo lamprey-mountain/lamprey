0f1854fea815f5662921230ee819a5c9b2f94916
tezlm <noreply@celery.eu.org>
2026-08-04 21:26:02 -0700

    chore: add readme to kerosene crate
    Signed-off-by: tezlm <noreply@celery.eu.org>

---
+++ kerosene/README.md
@@ -0,0 +1,5 @@
+# kerosene
+
+umbrella crate for kerosene (backend server for lamprey mountain)
+
+todo: write out this readme, link to crates, link to lamprey stuff
--- kerosene/src/lib.rs
+++ kerosene/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod server;
 
+// TODO: what actually goes here? nothing besides the readme?
+
 pub(crate) mod prelude {
     pub use lamprey_backend_core::prelude::*;
 }
