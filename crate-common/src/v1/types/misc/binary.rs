f371ac58c0924fddeb93bc93c7c024ab1b67cc25
tezlm <noreply@celery.eu.org>
2026-08-12 01:47:41 -0700

    feat: conversions for Binary
    Signed-off-by: tezlm <noreply@celery.eu.org>

--- crate-common/src/v1/types/misc/binary.rs
+++ crate-common/src/v1/types/misc/binary.rs
@@ -1,16 +1,38 @@
 use std::ops::Deref;
 
+use crate::v1::types::error::{ApiError, ApiResult, ErrorCode};
 use bytes::Bytes;
 
 /// some binary data
 ///
 /// serialized as unpaddeded url safe base64 for human readable formats (json)
 /// and raw binary otherwise (msgpack)
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Binary<const MAX_SIZE: usize>(pub Bytes);
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Binary<const MAX_LENGTH: usize>(pub Bytes);
 
 // TODO: struct BinaryUnlimited for unrestricted max size?
-// TODO: rename MAX_SIZE to MAX_LEN or MAX_LENGTH?
+
+impl<const MAX_LENGTH: usize> Binary<MAX_LENGTH> {
+    pub fn new(v: Vec<u8>) -> ApiResult<Self> {
+        if v.len() > MAX_LENGTH {
+            return Err(ApiError::with_message(
+                ErrorCode::InvalidData,
+                format!("length {} exceeds max length of {}", v.len(), MAX_LENGTH),
+            ));
+        }
+        Ok(Self(v.into()))
+    }
+
+    /// create a new binary without checking the length
+    ///
+    /// # Safety
+    ///
+    /// this does not cause undefined behavior, but it may violate the domain invariant
+    /// that the length is less than or equal to MAX_LENGTH
+    pub unsafe fn new_unchecked(v: Vec<u8>) -> Self {
+        Self(v.into())
+    }
+}
 
 #[cfg(feature = "utoipa")]
 mod _u {
@@ -22,8 +44,9 @@ mod _u {
 
     use crate::v1::types::misc::binary::Binary;
 
-    // TODO: indicate MAX_SIZE in schema?
-    impl<const MAX_SIZE: usize> PartialSchema for Binary<MAX_SIZE> {
+    // TODO: indicate MAX_LENGTH in schema?
+    // .description(format!("binary data (max len {})", MAX_LENGTH)),
+    impl<const MAX_LENGTH: usize> PartialSchema for Binary<MAX_LENGTH> {
         fn schema() -> RefOr<Schema> {
             RefOr::T(
                 AnyOf::builder()
@@ -42,7 +65,7 @@ mod _u {
         }
     }
 
-    impl<const MAX_SIZE: usize> ToSchema for Binary<MAX_SIZE> {}
+    impl<const MAX_LENGTH: usize> ToSchema for Binary<MAX_LENGTH> {}
 }
 
 #[cfg(feature = "serde")]
@@ -55,7 +78,7 @@ mod _s {
 
     use crate::v1::types::misc::binary::Binary;
 
-    impl<const MAX_SIZE: usize> Serialize for Binary<MAX_SIZE> {
+    impl<const MAX_LENGTH: usize> Serialize for Binary<MAX_LENGTH> {
         fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
         where
             S: serde::Serializer,
@@ -69,7 +92,7 @@ mod _s {
         }
     }
 
-    impl<'de, const MAX_SIZE: usize> Deserialize<'de> for Binary<MAX_SIZE> {
+    impl<'de, const MAX_LENGTH: usize> Deserialize<'de> for Binary<MAX_LENGTH> {
         fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
         where
             D: serde::Deserializer<'de>,
@@ -108,12 +131,12 @@ mod _s {
                 deserializer.deserialize_byte_buf(BinaryVisitor)?
             };
 
-            if bytes.len() > MAX_SIZE {
+            if bytes.len() > MAX_LENGTH {
                 // TODO: use api error
                 Err(de::Error::custom(format!(
-                    "length {} exceeds MAX_SIZE of {}",
+                    "length {} exceeds max length of {}",
                     bytes.len(),
-                    MAX_SIZE
+                    MAX_LENGTH
                 )))
             } else {
                 Ok(Binary(bytes))
@@ -122,13 +145,25 @@ mod _s {
     }
 }
 
-impl<const MAX_SIZE: usize> From<Binary<MAX_SIZE>> for Bytes {
-    fn from(b: Binary<MAX_SIZE>) -> Self {
+impl<const MAX_LENGTH: usize> From<Binary<MAX_LENGTH>> for Bytes {
+    fn from(b: Binary<MAX_LENGTH>) -> Self {
         b.0
     }
 }
 
-impl<const MAX_SIZE: usize> Deref for Binary<MAX_SIZE> {
+impl<const MAX_LENGTH: usize> From<Bytes> for Binary<MAX_LENGTH> {
+    fn from(b: Bytes) -> Self {
+        Self(b)
+    }
+}
+
+impl<const MAX_LENGTH: usize> From<Vec<u8>> for Binary<MAX_LENGTH> {
+    fn from(b: Vec<u8>) -> Self {
+        Self(b.into())
+    }
+}
+
+impl<const MAX_LENGTH: usize> Deref for Binary<MAX_LENGTH> {
     type Target = [u8];
 
     fn deref(&self) -> &Self::Target {
@@ -136,20 +171,8 @@ impl<const MAX_SIZE: usize> Deref for Binary<MAX_SIZE> {
     }
 }
 
-impl<const MAX_SIZE: usize> AsRef<[u8]> for Binary<MAX_SIZE> {
+impl<const MAX_LENGTH: usize> AsRef<[u8]> for Binary<MAX_LENGTH> {
     fn as_ref(&self) -> &[u8] {
         &self.0
     }
 }
-
-// TODO: impl these
-// impl<const MAX_SIZE: usize> Binary<MAX_SIZE> {
-//     pub fn new(v: Vec<u8>) -> ApiResult<Self> {
-//         todo!()
-//     }
-
-//     // is unsafe correct here?
-//     pub unsafe fn new_unchecked(v: Vec<u8>) -> Self {
-//         todo!()
-//     }
-// }
