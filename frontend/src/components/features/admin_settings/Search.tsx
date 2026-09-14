d4c06e505fc8e1fedb762e11576d0d4eb862e4ff
tezlm <noreply@celery.eu.org>
2026-09-14 06:54:31 -0700

    refactor(ui): use new reindex endpoint in admin ui
    Signed-off-by: tezlm <noreply@celery.eu.org>

--- frontend/src/components/features/admin_settings/Search.tsx
+++ frontend/src/components/features/admin_settings/Search.tsx
@@ -3,14 +3,14 @@ import { useApi } from "@/api";
 import { useModals } from "@/contexts/modal";
 
 export function Search() {
-	const api2 = useApi();
+	const api = useApi();
 	const [, modalCtl] = useModals();
 
 	const [stats, { refetch: refetchStats }] = createResource(() =>
-		api2.client.http.GET("/api/v1/admin/search/stats").then((r) => r.data),
+		api.client.http.GET("/api/v1/admin/search/stats").then((r) => r.data),
 	);
 	const [dlq, { refetch: refetchDlq }] = createResource(() =>
-		api2.client.http.GET("/api/v1/admin/search/dlq").then((r) => r.data),
+		api.client.http.GET("/api/v1/admin/search/dlq").then((r) => r.data),
 	);
 
 	const reindexEverything = () => {
@@ -18,8 +18,14 @@ export function Search() {
 			"Are you sure you want to reindex EVERYTHING? This will delete all existing search data first.",
 			(confirmed) => {
 				if (confirmed) {
-					api2.client.http
-						.POST("/api/v1/admin/reindex-everything")
+					api.client.http
+						.POST("/api/v1/admin/reindex", {
+							body: {
+								channel_ids: [],
+								doctypes: [],
+								room_ids: [],
+							},
+						})
 						.then(() => {
 							modalCtl.alert("Reindexing queued.");
 							refetchStats();
@@ -31,7 +37,7 @@ export function Search() {
 	};
 
 	const deleteDlq = (id: string) => {
-		api2.client.http
+		api.client.http
 			.DELETE("/api/v1/admin/search/dlq/{id}", {
 				params: { path: { id } },
 			})
@@ -39,7 +45,7 @@ export function Search() {
 	};
 
 	const retryDlq = (id: string) => {
-		api2.client.http
+		api.client.http
 			.POST("/api/v1/admin/search/dlq/{id}/retry", {
 				params: { path: { id } },
 			})
