aac63e91c16e4f731bfe840c08e9dd0fb53f3d4a
tezlm <noreply@celery.eu.org>
2026-09-19 15:06:41 -0700

    feat(macros): export route handler fn under module::handler
    Signed-off-by: tezlm <noreply@celery.eu.org>

--- crate-macros/src/handlers_new.rs
+++ crate-macros/src/handlers_new.rs
@@ -4,11 +4,13 @@ use syn::{ItemFn, Path, parse2};
 
 pub fn expand(args: TokenStream, item: TokenStream) -> syn::Result<TokenStream> {
     let metadata_path = parse2::<Path>(args)?;
-    let input = parse2::<ItemFn>(item)?;
+    let mut input = parse2::<ItemFn>(item)?;
 
-    let fn_name = &input.sig.ident;
-    let handler_name = format_ident!("__{}_handler", fn_name);
+    let fn_name = input.sig.ident.clone();
     let ep_type = quote! { #metadata_path::Endpoint };
+
+    // rename the actual fn to `handle`
+    input.sig.ident = format_ident!("handle");
 
     Ok(quote! {
         mod #fn_name {
@@ -20,17 +22,17 @@ pub fn expand(args: TokenStream, item: TokenStream) -> syn::Result<TokenStream> 
             #input
 
             pub fn register(r: &mut crate::Routes) {
-                async fn #handler_name(
+                async fn handler_inner(
                     req: crate::util::Req<#ep_type>,
                 ) -> Result<impl ::axum::response::IntoResponse> {
-                    #fn_name(req).await.map(|r| {
+                    handle(req).await.map(|r| {
                         r.encode().map(::axum::body::Body::from)
                     })
                 }
                 let meta = #ep_type::metadata();
                 r.route(
                     meta.path,
-                    ::axum::routing::on(meta.method.to_filter(), #handler_name),
+                    ::axum::routing::on(meta.method.to_filter(), handler_inner),
                 );
                 r.path(
                     meta.path,
