767ba2868f03b7643f06542a42a5dc87f4dbae05
tezlm <noreply@celery.eu.org>
2026-09-19 12:36:16 -0700

    feat(api)!: serialize time as binary for msgpack
    Signed-off-by: tezlm <noreply@celery.eu.org>

--- crate-common/src/v1/types/misc/time.rs
+++ crate-common/src/v1/types/misc/time.rs
@@ -5,28 +5,11 @@ use std::{
 
 use crate::v1::types::misc::duration::Duration as CommonDuration;
 
-#[cfg(feature = "serde")]
-use serde::{Deserialize, Serialize};
 use time::{OffsetDateTime, PrimitiveDateTime};
 
-#[cfg(feature = "utoipa")]
-use utoipa::ToSchema;
-
-// TODO: swap all date/time types to this
 /// A date, time, and timezone. Serialized to rfc3339.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(feature = "utoipa", derive(ToSchema))]
-pub struct Time(
-    #[cfg_attr(
-        feature = "serde",
-        serde(
-            serialize_with = "time::serde::rfc3339::serialize",
-            deserialize_with = "time::serde::rfc3339::deserialize"
-        )
-    )]
-    OffsetDateTime,
-);
+pub struct Time(OffsetDateTime);
 
 impl Time {
     pub fn now_utc() -> Self {
@@ -114,4 +97,73 @@ impl Sub<Time> for Time {
     fn sub(self, rhs: Time) -> Self::Output {
         self.0 - rhs.0
     }
+}
+
+#[cfg(feature = "serde")]
+mod _s {
+    use super::*;
+    use serde::{Deserialize, Serialize};
+
+    impl Serialize for Time {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer,
+        {
+            if serializer.is_human_readable() {
+                time::serde::rfc3339::serialize(&self.0, serializer)
+            } else {
+                serializer.serialize_i128(self.0.unix_timestamp_nanos())
+            }
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Time {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+        {
+            if deserializer.is_human_readable() {
+                time::serde::rfc3339::deserialize(deserializer).map(Time)
+            } else {
+                i128::deserialize(deserializer)
+                    .and_then(|t| {
+                        OffsetDateTime::from_unix_timestamp_nanos(t)
+                            .map_err(serde::de::Error::custom)
+                    })
+                    .map(Time)
+            }
+        }
+    }
+}
+
+#[cfg(feature = "utoipa")]
+mod _u {
+    use super::*;
+
+    use utoipa::{
+        PartialSchema, ToSchema,
+        openapi::{
+            ObjectBuilder, RefOr,
+            schema::{Schema, Type},
+        },
+    };
+
+    impl PartialSchema for Time {
+        fn schema() -> RefOr<Schema> {
+            RefOr::T(
+            ObjectBuilder::new()
+                .schema_type(Type::String)
+                .description(Some(
+                    "A datetime in rfc3339 format for json.\n\nIs a i128 unix timestamp in nanoseconds for binary formats.",
+                ))
+                .examples([
+                    serde_json::json!("2025-07-20T18:04:54.309625Z"),
+                ])
+                .build()
+                .into()
+            )
+        }
+    }
+
+    impl ToSchema for Time {}
 }
