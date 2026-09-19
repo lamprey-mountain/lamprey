564b13c2a450e0cf359da4475ec8283fb3164d1e
tezlm <noreply@celery.eu.org>
2026-09-19 12:56:57 -0700

    fix(api): accept both timestamp and string for Time
    Signed-off-by: tezlm <noreply@celery.eu.org>

--- crate-common/src/v1/types/misc/time.rs
+++ crate-common/src/v1/types/misc/time.rs
@@ -122,16 +122,56 @@ mod _s {
         where
             D: serde::Deserializer<'de>,
         {
-            if deserializer.is_human_readable() {
-                time::serde::rfc3339::deserialize(deserializer).map(Time)
-            } else {
-                i128::deserialize(deserializer)
-                    .and_then(|t| {
-                        OffsetDateTime::from_unix_timestamp_nanos(t)
-                            .map_err(serde::de::Error::custom)
-                    })
-                    .map(Time)
+            struct TimeVisitor;
+
+            impl<'de> serde::de::Visitor<'de> for TimeVisitor {
+                type Value = Time;
+
+                fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                    f.write_str("an RFC3339 string or a unix timestamp in nanoseconds")
+                }
+
+                fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+                where
+                    E: serde::de::Error,
+                {
+                    OffsetDateTime::parse(v, &time::format_description::well_known::Rfc3339)
+                        .map(Time)
+                        .map_err(E::custom)
+                }
+
+                fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
+                where
+                    E: serde::de::Error,
+                {
+                    self.visit_i128(v as i128)
+                }
+
+                fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+                where
+                    E: serde::de::Error,
+                {
+                    self.visit_i128(v as i128)
+                }
+
+                fn visit_i128<E>(self, v: i128) -> Result<Self::Value, E>
+                where
+                    E: serde::de::Error,
+                {
+                    OffsetDateTime::from_unix_timestamp_nanos(v)
+                        .map(Time)
+                        .map_err(E::custom)
+                }
+
+                fn visit_u128<E>(self, v: u128) -> Result<Self::Value, E>
+                where
+                    E: serde::de::Error,
+                {
+                    self.visit_i128(v as i128)
+                }
             }
+
+            deserializer.deserialize_any(TimeVisitor)
         }
     }
 }
